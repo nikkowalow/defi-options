fb01460046e3db5893031665764d9160c59d8b0e
Davis Gramza <davisgramza@gmail.com>
2021-02-11 16:26:10 -0600

    Created option class prototype.

--- src/components/option.tsx
+++ src/components/option.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import rp from 'request-promise';
 import * as config from '../configs/config.json';
-import { useTable } from 'react-table';
-
-import { Link } from 'react-router-dom';
-import { throws } from 'assert';
-import { timeStamp } from 'console';
 import { Coin } from '../models/coin'
 
 interface OptionProps {
@@ -16,49 +11,9 @@ export class Option extends React.Component<OptionProps> {
 
     makeOption() {
         return (
-            <table>
-                <thead>
-                    <tr >
-                        <th>Strike</th>
-                        <th>Delta</th>
-                        <th>Gamma</th>
-                        <th>Theta</th>
-                        <th>Rho</th>
-                        <th>Vega</th>
-                        <th>Change</th>
-                        <th>% Change</th>
-                        <th>Volume</th>
-                        <th>Interest</th>
-                        <th>Imp. Vol</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                    </tr>
-                    <tr>
-                        <td>Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                    </tr>
-                    <tr>
-                        <td>Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                    </tr>
-                    <tr>
-                        <td>Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
-                    </tr>
-                </tbody>
-            </table >
+            <div>
+                 
+            </div>
         );
     }
 
