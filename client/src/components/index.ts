027a210267692d14ab1937df37b984b9f8bf9234
Nikko Kowalow <nikko@kowalow.com>
2021-02-10 22:39:21 -0600

    removed client folder. not needed.

--- client/src/components/index.ts
+++
@@ -1,8 +0,0 @@
-
-export * from './connectWallet'
-export * from './disconnectButton'
-export * from './connectButton'
-export * from './navigationBar'
-export * from './coins'
-export * from './optionChain'
-export * from './option'
--- client/src/components/quote.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export class Quote extends Component {
-  render() {
-    return (
-      <div></div>
-    );
-  }
-}
